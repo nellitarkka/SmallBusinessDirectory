7a902fb24d7e23512b2465408a7ad393117e78e6
nellitarkka <nelli_tarkka@hotmail.com>
2026-02-03 16:05:01 +0100

    Update api.ts for deployment
    it was set to only localhost and since deploying i had to change it

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const API_ORIGIN = API_BASE_URL.replace(/\/api$/, '');
 
 // Helper to get token from localStorage
